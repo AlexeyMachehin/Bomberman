095597dbc027ef80e692c0abac1bfa372e673444
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-03-18 03:00:16 +0500

    j

--- packages/client/src/utils/tests/snapshotTests/LeaderBoardPage.test.tsx
+++ packages/client/src/utils/tests/snapshotTests/LeaderBoardPage.test.tsx
@@ -2,12 +2,15 @@ import LiderBoardPage from '@/pages/leaderBoardPage/LeaderBoardPage';
 import { render } from '@testing-library/react';
 import { store } from '@/store/store';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
 test('LeaderBoardPage renders correctly', () => {
   const component = render(
-    <Provider store={store}>
-      <LiderBoardPage />
-    </Provider>
+    <MemoryRouter>
+      <Provider store={store}>
+        <LiderBoardPage />
+      </Provider>
+    </MemoryRouter>
   );
   expect(component).toMatchSnapshot();
 });
--- packages/client/src/utils/tests/snapshotTests/StartPageButton.tsx
+++ packages/client/src/utils/tests/snapshotTests/StartPageButton.tsx
@@ -2,12 +2,15 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 import { StartPageButton } from '@/features/startPageButton/StartPageButton';
+import { MemoryRouter } from 'react-router-dom';
 
 test('StartPageButton component renders correctly', () => {
   const component = render(
-    <Provider store={store}>
-      <StartPageButton />
-    </Provider>
+    <MemoryRouter>
+      <Provider store={store}>
+        <StartPageButton />
+      </Provider>
+    </MemoryRouter>
   );
   expect(component).toMatchSnapshot();
 });
