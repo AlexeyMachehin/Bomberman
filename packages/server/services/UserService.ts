e8ba347d8b6a04bf9cca91291914990a9139839e
goliaeth <goliaeth@gmail.com>
2023-03-20 10:26:48 +0500

    fixes update of user info in db on startup

--- packages/server/services/UserService.ts
+++ packages/server/services/UserService.ts
@@ -3,7 +3,7 @@ import { User } from '../models/User';
 class UserService {
   async createUser(payload: { userId?: number; userName?: string }) {
     if (!payload.userId || !payload.userName) return;
-    const newUser = await User.create({
+    const newUser = await User.upsert({
       id: payload.userId,
       name: payload.userName,
     });
